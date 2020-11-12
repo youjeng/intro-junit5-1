dc784113f56643d56b512bca466c2c953a7571dd
Michael Cheich <michaelcheich@Michaels-iMac-2.local>
2020-11-11 21:29:12 -0500

    added before all and after all tests

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -2,6 +2,9 @@ package guru.springframework;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,8 +12,14 @@ class GreetingTest {
 
 	private Greeting result;
 	
+	@BeforeAll
+	public static void beforeClass() {
+		System.out.println("King of the java code!");
+	}
+	
 	@BeforeEach
 	void setUp() {
+		System.out.println("eh man!");
 		result = new Greeting();
 	}
 	
@@ -25,4 +34,14 @@ class GreetingTest {
 		assertEquals("Hello Mike", result.helloWorld("Mike"));
 	}
 
+	@AfterEach
+	void tearDown() {
+		System.out.println("out you go!");
+	}
+	
+	@AfterAll
+	public static void afterClass() {
+		System.out.println("I get teh last word!");
+	}
+	
 }
