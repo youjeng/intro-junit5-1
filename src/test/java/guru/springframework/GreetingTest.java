b9808c5dfd384d88ad95416d1fe3b7d9178a8e30
Michael Cheich <michaelcheich@Michaels-iMac-2.local>
2020-11-11 21:21:02 -0500

    added before each setup function

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -2,19 +2,26 @@ package guru.springframework;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GreetingTest {
 
+	private Greeting result;
+	
+	@BeforeEach
+	void setUp() {
+		result = new Greeting();
+	}
+	
+	
 	@Test
 	void testHelloWorld() {
-		Greeting result = new Greeting();
 		assertEquals("Hello World", result.helloWorld());
 	}
 
 	@Test
 	void testHelloWorldString() {
-		Greeting result = new Greeting();
 		assertEquals("Hello Mike", result.helloWorld("Mike"));
 	}
 
